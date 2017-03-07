0ab7b8de86a6cb58e586fe291e91f83764a5bfec
sballmer <ballmer.stephane@gmail.com>
2017-03-07 12:30:30 +0100

    added background, spaceship and it's arrow controller

---
+++ Project/5.LaserDefender/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour 
+{
+	public float speed = 10.0f;
+
+	float xmin = -5, xmax = 5;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+		{
+			// adding offset
+			Vector3 pos = this.transform.position + Vector3.left * speed * Time.deltaTime;
+
+			// clamping to the screen
+			pos.x = Mathf.Clamp(pos.x, xmin, xmax);
+
+			// setting position
+			this.transform.position = pos;
+		}
+		else if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+		{
+			// adding offset
+			Vector3 pos = this.transform.position + Vector3.right * speed * Time.deltaTime;
+			
+			// clamping to the screen
+			pos.x = Mathf.Clamp(pos.x, xmin, xmax);
+			
+			// setting position
+			this.transform.position = pos;
+		}
+	}
+}
