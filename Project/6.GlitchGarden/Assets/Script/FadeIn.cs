55f22b92e080e81c487e39c56b0b6d51d1e6d338
sballmer <ballmer.stephane@gmail.com>
2017-03-08 19:35:04 +0100

    add project Glitch garden, add scenes, set menus, add playerperfs

---
+++ Project/6.GlitchGarden/Assets/Script/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour 
+{
+	public float fadeInTime = 2f; // in sec
+	
+	private Color currentColor = Color.black;
+	private Image fadePanel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fadePanel = this.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Time.timeSinceLevelLoad <= fadeInTime) {
+			currentColor.a = 1 - Time.timeSinceLevelLoad / fadeInTime;
+			fadePanel.color = currentColor;
+		} else {
+			this.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Project/6.GlitchGarden/Assets/Script/PlayerPreferencesManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPreferencesManager : MonoBehaviour 
+{
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+
+	public static void setMasterVolume ( float volume)
+	{
+		if (volume > 0f && volume < 1f)
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		else
+			Debug.LogError ("Master volume out of range");
+	}
+
+	public static float getMasterVolume()
+	{
+		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+	}
+}
