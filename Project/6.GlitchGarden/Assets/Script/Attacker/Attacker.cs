042ce9096e8d80d8f91f8d3434754afc23e0b904
sballmer <ballmer.stephane@gmail.com>
2017-03-10 11:42:15 +0100

    correct attackers and defender, start working on behavior (fox jump over stone now)

---
+++ Project/6.GlitchGarden/Assets/Script/Attacker/Attacker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour 
+{
+	[Range (0f, 1f)] 
+	public float walkSpeed;
+
+	private Rigidbody2D rigidBody;
+	private GameObject attackingTarget;
+	private const float periodAttack = 1f; // 1 hz
+
+	// Use this for initialization
+	void Start () 
+	{
+		attackingTarget = null;
+		rigidBody = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log ("Trigg" + name);
+	}
+
+	void setSpeed(float speed = 0f)
+	{
+		walkSpeed = speed;
+		rigidBody.velocity = new Vector3 ( -walkSpeed, 0f, 0f);
+	}
+
+	public void setSpeedFactor(float factor)
+	{
+		rigidBody.velocity = new Vector3 ( -walkSpeed * factor, 0f, 0f);
+	}
+
+	public void StrikeCurrentTarget()
+	{
+		Debug.Log (name + " strike his current target");
+	}
+
+	public void aggress(GameObject target)
+	{
+		attackingTarget = target;
+
+		// Invoke (attackingTarget, periodAttack); // invoque periodiqualy the dammage, when do you cancel it ?
+	}
+}
---
+++ Project/6.GlitchGarden/Assets/Script/Attacker/Fox.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Attacker))]
+public class Fox : MonoBehaviour 
+{
+	private Animator anim;
+	private Attacker attacker;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = this.GetComponent<Animator> ();
+		attacker = this.GetComponent<Attacker> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		GameObject collideWith = collider.gameObject;
+
+		if (!collideWith.GetComponent<Defender> ()) 
+			return;
+		else if (collideWith.GetComponent<Stone> ())
+			anim.SetTrigger ("Jump");
+		else 
+		{
+			attacker.aggress(collideWith);
+		}
+
+	}
+}
