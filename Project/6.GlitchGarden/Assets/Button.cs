a505e79ae6bfd7e5d6299dc405f6ad4ec1abaa93
sballmer <ballmer.stephane@gmail.com>
2017-03-12 22:42:23 +0300

    added possibility to selected defender on the menu

--- Project/6.GlitchGarden/Assets/Button.cs
+++ Project/6.GlitchGarden/Assets/Button.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Button : MonoBehaviour {
+public class Button : MonoBehaviour 
+{
+	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
 
-	// Use this for initialization
-	void Start () {
-	
+	private Button[] allButtons;
+
+	void Start()
+	{
+		allButtons = FindObjectsOfType<Button> ();
+
+		GetComponent<SpriteRenderer> ().color = Color.black;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnMouseDown()
+	{
+		foreach (Button element in allButtons) 
+		{
+			element.GetComponent<SpriteRenderer> ().color = Color.black;
+		}
+
+		GetComponent<SpriteRenderer> ().color = Color.white;
+		selectedDefender = defenderPrefab;
 	}
 }
