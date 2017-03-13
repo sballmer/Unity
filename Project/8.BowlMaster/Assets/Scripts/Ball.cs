b3df27ad6b89877d2129f5c0fd51522ffa11e50b
sballmer <ballmer.stephane@gmail.com>
2017-03-13 20:03:25 +0100

    add pins, floor, ball

---
+++ Project/8.BowlMaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour 
+{
+	public float launchSpeed;
+
+	private Rigidbody myRigidBody;
+	private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody> ();
+		myAudioSource = GetComponent<AudioSource> ();
+
+		Launch ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void Launch ()
+	{
+		myRigidBody.velocity = new Vector3 (0f, 0f, launchSpeed);
+		myAudioSource.Play ();
+	}
+}
