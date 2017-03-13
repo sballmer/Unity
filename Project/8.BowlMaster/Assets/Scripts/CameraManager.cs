e0f8893fb1e7a405a16b8b252aeefc5f74b25bd5
sballmer <ballmer.stephane@gmail.com>
2017-03-13 21:05:12 +0300

    camera following bowl

---
+++ Project/8.BowlMaster/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour 
+{
+	public GameObject toFollow;
+
+	private Vector3 him2Cam;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (toFollow)
+			him2Cam = this.transform.position - toFollow.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (toFollow)
+			this.transform.position = toFollow.transform.position + him2Cam;
+	}
+}
