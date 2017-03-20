8024ca51d25acd53f0c1d5b5b1eb7e6440cb1047
sballmer <ballmer.stephane@gmail.com>
2017-03-20 13:10:46 +0100

    added helicopter, sounds and random player span positions

---
+++ Project/9.ZombieRunner/Assets/Scripts/Eyes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour 
+{
+    private Camera eyesCamera;
+    private float defaultFOV;
+
+	// Use this for initialization
+	void Start () 
+    {
+        eyesCamera = GetComponent<Camera> ();
+        defaultFOV = eyesCamera.fieldOfView;
+	}
+
+    void Update()
+    {
+        if (Input.GetButton("Zoom"))
+            eyesCamera.fieldOfView = defaultFOV / 1.5f;
+        else
+            eyesCamera.fieldOfView = defaultFOV;
+    }
+}
---
+++ Project/9.ZombieRunner/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+    public GameObject playerSpawPoint;
+    public bool respawnButton;
+
+    private Transform[] spawnPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+        respawnButton = false;
+        spawnPos = playerSpawPoint.GetComponentsInChildren<Transform>();
+
+        SetPosToARandomStartingPoint();
+	}
+
+    void Update()
+    {
+        if (respawnButton)
+        {
+            ReSpawn();
+            respawnButton = false;
+        }
+    }
+
+    void SetPosToARandomStartingPoint()
+    {
+        int index = Random.Range(1, spawnPos.Length); // position 0 make the camera falling to the infinite... 
+        transform.position = spawnPos[index].position;
+        transform.rotation = spawnPos[index].rotation;
+    }
+
+    public void ReSpawn()
+    {
+        SetPosToARandomStartingPoint();
+    }
+}
