3d61dfd8359e33cd319d99a86257461f6176f0f7
sballmer <ballmer.stephane@gmail.com>
2017-03-20 16:37:07 +0300

    add daytime cycle and allow VR on occulus rift

---
+++ Project/9.ZombieRunner/Assets/Scripts/DayCycle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour 
+{
+    [Tooltip ("seconds in the game per real seconds, Try 60")]
+    public float timeScale;
+
+    private Quaternion startQuaternion;
+
+	// Use this for initialization
+	void Start () 
+    {
+        startQuaternion = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.RotateAround (transform.position, Vector3.forward, Time.deltaTime / 360f * timeScale);
+	}
+
+    void restSun()
+    {
+        transform.rotation = startQuaternion;
+    }
+}
